319796e84d006c2c2803e1f5629fe5554bc6b3c3
liuyz <947917881@qq.com>
2021-05-17 17:21:40 +0800

    study rabbit

--- rabbitmq-order/src/main/java/com/rabbitmqorder/rabbitmqorder/service/OrderServiceImpl.java
+++ rabbitmq-order/src/main/java/com/rabbitmqorder/rabbitmqorder/service/OrderServiceImpl.java
@@ -51,7 +51,7 @@ public class OrderServiceImpl {
     }
 
     //创建rabbitmq确认回调机制
-    //被PostConstruct修饰的方法，会在加载Servlet的时候执行，且只会执行一次
+    //被PostConstruct修饰的方法regCallBack()，会在加载Servlet的时候执行，且只会执行一次
     //构造函数之后，init方法之前执行（所有的Bean加载完成）
     @PostConstruct
     public void regCallBack() {
